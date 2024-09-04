dc5956749fc88c80f688dbf976364d6917c4087f
wannabeSE <samirk724@gmail.com>
2024-09-05 02:30:44 +0600

    added/ audio_service library with config, getx service

--- lib/getx_controllers/song_controller.dart
+++ lib/getx_controllers/song_controller.dart
@@ -5,26 +5,24 @@ import 'package:flutter_music_player/services/test_permission_handler.dart';
 import 'package:get/get.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
-class SongController extends GetxController{
-
+class SongController extends GetxController {
   RxList<MediaItem> deviceSongs = <MediaItem>[].obs;
   final List<MediaItem> _songs = [];
   final OnAudioQuery audioQuery = OnAudioQuery();
 
-  Future getDeviceSongs()async{
-    try{
+  Future getDeviceSongs() async {
+    try {
       await requestPermission();
       final List songModels = await audioQuery.querySongs();
-      for(final SongModel songModel in songModels){
+      for (final SongModel songModel in songModels) {
         final MediaItem song = await songModelToMediaItem(songModel);
         _songs.add(song);
       }
       deviceSongs.value = _songs;
       return _songs;
-    }catch(e){
+    } catch (e) {
       debugPrint('Error fetching device songs $e');
       deviceSongs([]);
     }
-
   }
 }
--- lib/getx_services/audio_player_getx_service.dart
+++ lib/getx_services/audio_player_getx_service.dart
@@ -4,27 +4,25 @@ import 'package:flutter_music_player/getx_controllers/song_controller.dart';
 import 'package:flutter_music_player/services/audio_player_handler.dart';
 import 'package:get/get.dart';
 
-class AudioPlayerService extends GetxService{
-
+class AudioPlayerService extends GetxService {
   late JustAudioPlayerHandler justAudioPlayerHandler;
   final songController = Get.put(SongController());
 
-  Future<void> init()async{
+  Future<void> init() async {
     justAudioPlayerHandler = await AudioService.init(
-      builder: () => JustAudioPlayerHandler(),
-      config: const AudioServiceConfig(
-        androidNotificationChannelId: 'com.boom.app',
-        androidNotificationChannelName: 'Boom',
-        androidShowNotificationBadge: true,
-        androidNotificationOngoing: true
-      )
-    );
+        builder: () => JustAudioPlayerHandler(),
+        config: const AudioServiceConfig(
+            androidNotificationChannelId: 'com.boom.app',
+            androidNotificationChannelName: 'Boom',
+            androidShowNotificationBadge: true,
+            androidNotificationOngoing: true));
   }
-  Future<void> loadSongs()async{
-    try{
+
+  Future<void> loadSongs() async {
+    try {
       final loadedSongs = await songController.getDeviceSongs();
       await justAudioPlayerHandler.initSongs(loadedSongs);
-    }catch(e){
+    } catch (e) {
       debugPrint('Error loading songs $e');
     }
   }
